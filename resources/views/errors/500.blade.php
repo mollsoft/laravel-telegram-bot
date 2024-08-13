fb11e9c4d0c021f44622ddc3c5335247e19ef9ab
MollSoft <info@mollsoft.com>
2024-08-13 14:45:54 +0300

    Fix bug

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,9 +3,3 @@
     <line>При обработке Вашего запроса возникла техническая ошибка.</line>
     <line>Пожалуйста, повторите запрос позже!</line>
 </message>
-
-@if( isset( $exception ) && $exception instanceof Exception )
-<message>
-    <line><code>{{ $exception->getMessage() }}</code></line>
-</message>
-@endif
