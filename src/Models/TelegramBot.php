6802a43d135bb5bb6a4b0ff4ecc8f3a50f64235a
MollSoft <info@mollsoft.com>
2025-01-29 18:40:56 +0300

    Telegram Bot add hasMany for chats

--- src/Models/TelegramBot.php
+++ src/Models/TelegramBot.php
@@ -3,7 +3,10 @@
 namespace Mollsoft\Telegram\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Mollsoft\Telegram\API;
+use Mollsoft\Telegram\Facades\Telegram;
 
 class TelegramBot extends Model
 {
@@ -23,5 +26,11 @@ class TelegramBot extends Model
     public function api(): API
     {
         return new API($this->token);
+    }
+
+    public function chats(): HasMany
+    {
+        /** @phpstan-ignore-next-line */
+        return $this->hasMany(Telegram::chatModel());
     }
 }
